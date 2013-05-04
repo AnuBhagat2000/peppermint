a30c915889a21a1eb5ac2095c6b81c49cad652a3
irene skup smith <irene.s.smith@nasa.gov>
2013-05-03 21:29:58 -0700

    submittal

--- smithCalculator/src/com/example/calculator/SmithCalcMainActivity.java
+++ smithCalculator/src/com/example/calculator/SmithCalcMainActivity.java
@@ -12,88 +12,91 @@ import android.widget.Toast;
 
 public class SmithCalcMainActivity extends Activity {
 
-	private static final double seminal = -99999.999;
-	private static final String converter = "%.3f";
-	
+	private static final double seminal = -999.99;
+	private TextView operatorView = null;
+	private TextView left = null;
+	private TextView right = null;
+	private TextView displayAnsw = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		operatorView = (TextView) findViewById(R.id.operatorView);
+		left = (TextView) findViewById(R.id.leftOperand);
+		right = (TextView) findViewById(R.id.rightOperand);
+		displayAnsw = (TextView) findViewById(R.id.displayAnswer);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		TextView operatorView = (TextView) findViewById(R.id.operatorView);
 		operatorView.setTag(0); 
 		return true;
 	}
 
+	/** Clear.*/
+	public void onClear(View view) {
+		left.setText("");
+		right.setText("");
+		displayAnsw.setText("");
+	}
+
 	/** Copies from the operator button to the operator readonly view.*/
 	public void onClickOperator(View view) {
-		TextView operatorView = (TextView) findViewById(R.id.operatorView);
 		final Button button = (Button) findViewById(view.getId());
 		String buttonText = String.valueOf(button.getText());
 		operatorView.setText(buttonText);
-		operatorView.setTag(button.getId()); // save the operator
+		operatorView.setTag(button.getId()); // tag the operator
 	}
 
+	/** Go */
 	public void onClickAnswer(View view) {
-		final TextView left = (TextView) findViewById(R.id.leftOperand);
-		final TextView right = (TextView) findViewById(R.id.rightOperand);
-		final TextView displayAnsw = (TextView) findViewById(R.id.displayAnswer);
 
 		String leftText = String.valueOf(left.getText());
 		String rightText = String.valueOf(right.getText());
-		
 		double leftDouble = seminal;
 		double rDouble = seminal;
-		
+
 		try {
 			leftDouble = Double.parseDouble(leftText);
 		} catch (Exception e) {
+			onClear(view);
 			CharSequence c = String.format(Locale.US,"Left operand could not be converted.");
 			Toast.makeText(getApplicationContext(), c, Toast.LENGTH_SHORT).show();
-			e.printStackTrace();
+			return;
 		}
 		try {
 			rDouble = Double.parseDouble(rightText);
 		} catch (Exception e) {
+			onClear(view);
 			CharSequence c = String.format("Right operand could not be converted - ensure it is a number.");
 			Toast.makeText(getApplicationContext(), c, Toast.LENGTH_SHORT).show();
-			e.printStackTrace();
+			return;
 		}
-		
+
 		double res = seminal;
-		TextView operatorView = (TextView) findViewById(R.id.operatorView);
-		
-			switch ((Integer)operatorView.getTag()){
-			case R.id.buttonAdd:{
-				res = leftDouble + rDouble;
-				break;
-			}
-			case R.id.buttonSubtract:{
-				res = leftDouble - rDouble;
-				break;
-			}
-			case R.id.buttonMultiply:{
-				res = leftDouble * rDouble;
-				break;
-			}
-			case R.id.buttonDivide:{
-				res = leftDouble / rDouble;
-				break;
-			}
-            default: {
-            	break;
-            }
-			}
-	
-
+		switch ((Integer)operatorView.getTag()){
+		case R.id.buttonAdd:{
+			res = leftDouble + rDouble;
+			break;
+		}
+		case R.id.buttonSubtract:{
+			res = leftDouble - rDouble;
+			break;
+		}
+		case R.id.buttonMultiply:{
+			res = leftDouble * rDouble;
+			break;
+		}
+		case R.id.buttonDivide:{
+			res = leftDouble / rDouble;
+			break;
+		}
+		default: {
+			break;
+		}
+		}
 		displayAnsw.setText(String.valueOf(res));
-
 	}
-
-	
 }
